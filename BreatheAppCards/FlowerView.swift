fdd138ba8665cb361df1254aae9c47315bd7c2a9
Arlind Aliu <arlindaliu.dev@gmail.com>
2019-11-27 12:32:08 +0100

    renaming

--- BreatheAppCards/FlowerView.swift
+++ BreatheAppCards/FlowerView.swift
@@ -9,7 +9,7 @@
 import Foundation
 import SwiftUI
 
-struct GradientView: View {
+struct GradientCircle: View {
     var body: some View {
         let color1 = Color.init(red: 81/255, green: 204/255, blue: 159/255)
         let color2 = Color.init(red: 154/255, green: 255/255, blue: 248/255)
@@ -26,7 +26,7 @@ struct FlowerAnimatableView: View {
     var scale: CGFloat
     
     var body: some View {
-        Color.clear.overlay(Color.clear.modifier(FlowerAnimatableViewModifier(sides: sides,
+        Color.clear.overlay(Color.clear.modifier(FlowerAnimatableViewModifier(petalCount: sides,
                                                                                size: size,
                                                                                scale: scale)))
     }
@@ -34,14 +34,14 @@ struct FlowerAnimatableView: View {
 
 
 struct FlowerAnimatableViewModifier: AnimatableModifier {
-    var sides: Double
+    var petalCount: Double
     var size: CGFloat
     var scale: CGFloat
 
     var animatableData: AnimatablePair<Double, CGFloat> {
-        get {return AnimatablePair(sides, scale)}
+        get {return AnimatablePair(petalCount, scale)}
         set {
-            sides = newValue.first
+            petalCount = newValue.first
             scale = newValue.second
         }
     }
@@ -50,33 +50,33 @@ struct FlowerAnimatableViewModifier: AnimatableModifier {
         return CGSize(width: size*scale, height: size*scale)
     }
     
+    private var circleSize: CGFloat {
+        return (rect.width + (1-scale)*rect.width)/2
+    }
+    
+    private var radius: Double {
+           return Double(min(rect.width, rect.height))/4.0
+    }
+    
     func body(content: Content) -> some View {
-        let extra: Int = Double(sides) != Double(Int(sides)) ? 1 : 0
-        let numberOfSides = Int(sides) + extra
+        let extra: Int = Double(petalCount) != Double(Int(petalCount)) ? 1 : 0
+        let numberOfSides = Int(petalCount) + extra
         
         return ZStack {
             ForEach(Array(0..<numberOfSides), id: \.self) { i in
-                return GradientView()
-                    .frame(width: self.circleWidth(), height: self.circleWidth())
+                return GradientCircle()
+                    .frame(width: self.circleSize, height: self.circleSize)
                     .opacity(self.getOpacity(at: i))
                     .offset(x: self.getOffset(at: i).x, y: self.getOffset(at: i).y)
             }
         }
     }
     
-    func circleWidth() -> CGFloat {
-        return (rect.width + (1-scale)*rect.width)/2
-    }
-    
     func getOpacity(at i: Int) -> Double {
         let maxOpacity = 0.55
-        var opacity = sides - Double(i)
+        var opacity = petalCount - Double(i)
         opacity = max(0, min(1, opacity))
         return opacity*maxOpacity
-    }
-    
-    func radius() -> Double {
-        return Double(min(rect.width, rect.height))/4.0
     }
     
     func getOffset(at side: Int) -> CGPoint {
@@ -86,24 +86,23 @@ struct FlowerAnimatableViewModifier: AnimatableModifier {
     }
     
     func getOffset(_ center: CGPoint) -> CGPoint {
-        let r = radius()
-        return CGPoint(x: Double(center.x) - r/2, y: Double(center.y) - r/2)
+        return CGPoint(x: Double(center.x) - radius/2, y: Double(center.y) - radius/2)
     }
     
     func getPetalCenteres() -> [CGPoint] {
-        let h = radius()
+        let h = radius
         let center = CGPoint(x: h/2, y: h/2)
         
         var points: [CGPoint] = []
         
-        let extra: Int = Double(sides) != Double(Int(sides)) ? 1 : 0
+        let extra: Int = Double(petalCount) != Double(Int(petalCount)) ? 1 : 0
         
-        for i in 0..<Int(sides) + extra {
-            let angle = (Double(i) + Double(self.scale)) * (360/Double(sides))
+        for i in 0..<Int(petalCount) + extra {
+            let angle = (Double(i) + Double(self.scale)) * (360/Double(petalCount))
             let angleInRad = angle*Double.pi/180
             
-            let pt = CGPoint(x: center.x + CGFloat(cos(angleInRad) * h), y: center.y + CGFloat(sin(angleInRad)*h))
-            points.append(pt)
+            let point = CGPoint(x: center.x + CGFloat(cos(angleInRad) * h), y: center.y + CGFloat(sin(angleInRad)*h))
+            points.append(point)
         }
         return points
     }
@@ -112,7 +111,9 @@ struct FlowerAnimatableViewModifier: AnimatableModifier {
 #if DEBUG
 struct FlowerContentView_Previews : PreviewProvider {
     static var previews: some View {
-        ContentView().background(Color.black)
+        Color.black.overlay(
+        FlowerAnimatableView(sides: 6, size: 400, scale: 1.0)
+            .frame(width: 400, height: 400)).background(Color.red)
     }
 }
 #endif
